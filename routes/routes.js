cb94f23738f9adec1ef0c5cd5ad6b3574a7f7d21
Irina Shumak <spunovaiv@rambler.ru>
2022-11-08 22:48:24 +0300

    feature: back-end part

--- routes/routes.js
+++ routes/routes.js
@@ -14,7 +14,7 @@ router.post('/signin', celebrate({
 }), login);
 router.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email().unique(),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30).required(),
   }),
